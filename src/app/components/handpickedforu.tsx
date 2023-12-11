453350daf36b3d2142db132704b7b45a9cead567
theCephas <oshoiseoluwa@gmail.com>
2023-12-11 19:21:07 +0100

    Updates

--- src/app/components/handpickedforu.tsx
+++ src/app/components/handpickedforu.tsx
@@ -10,16 +10,86 @@ import Item6 from "../../../public/item6.svg";
 import Item7 from "../../../public/item7.svg";
 import Image from "next/image";
 import ProductCard from "../productcard/ProductCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Handpicked: React.FC = () => {
+  const settings = {
+    className: "center",
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 8,
+    swipeToSlide: true,
+    afterChange: function (index: number) {
+      console.log(
+        `Slider Changed to: ${index + 1}, background: #222; color: #bada55`
+      );
+    },
+  };
   return (
     <div className="pl-8 lg:pl-10 ">
       <div>
         <p>Handpicked for you</p>
       </div>
-      <div className="py-5">
-        <ProductCard imageSrc={Item1} name="Nike Air Sneakers" price={17000} />
-      </div>
+      <Slider {...settings}>
+        <div>
+          <ProductCard
+            imageSrc={Item1}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+        <div>
+          <ProductCard
+            imageSrc={Item2}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+        <div>
+          <ProductCard
+            imageSrc={Item3}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+        <div>
+          <ProductCard
+            imageSrc={Item4}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+        <div>
+          <ProductCard
+            imageSrc={Item5}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+        <div>
+          <ProductCard
+            imageSrc={Item6}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+        <div>
+          <ProductCard
+            imageSrc={Item6}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+        <div>
+          <ProductCard
+            imageSrc={Item6}
+            name="Nike Air Sneakers"
+            price={17000}
+          />
+        </div>
+      </Slider>
     </div>
   );
 };
