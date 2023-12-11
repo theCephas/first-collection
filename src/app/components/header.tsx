5abb64f8b48c2727670dc5f8d645c37acda0a1c2
theCephas <oshoiseoluwa@gmail.com>
2023-12-11 02:30:39 +0100

    Fixing the navabar

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,31 +1,40 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Logo from "../../../public/logo.svg";
 import User from "../../../public/user.svg";
 import Cart from "../../../public/cart.svg";
 import Image from "next/image";
 import Link from "next/link";
+import "../../../src/fonts.css";
+import { ChevronDown, ChevronUp, AlignJustify, X } from "lucide-react";
+import { motion, useCycle, AnimatePresence, MotionConfig } from "framer-motion";
 
 const Header = () => {
+  const [mobileNav, toggleMobileNav] = useCycle(false, true);
+  const [isDropDownVisible, setIsDropDownVisible] = useState(false);
+
   return (
-    <div className="px-8 lg:px-10 pt-4">
-      <div className="bg-[#000] font-judson w-[100%] m-auto mb-10 p-[16px] flex justify-between items-center rounded-[16px] backdrop-blur-[10px] ">
+    <div className="pt-4 gilroy fixed w-full z-20">
+      <div className="bg-[#000] mb-10 p-[16px] flex justify-between items-center rounded-[16px] backdrop-blur-[10px] mx-8 lg:mx-10 pt-4  ">
         <Image src={Logo} alt="Logo" width={0} />
-        <form className="relative">
+        <form className="relative hidden md:flex">
           <label className="hidden">Search</label>
           <input
             type="text"
             placeholder="Search for products"
-            className="rounded-[6px] outline-none border border-[#b3b3b3] md:w-[358px] h-[44px] text-[#b3b3b3] bg-[#000] flex py-[8px] px-[12px] items-center justify-center gap-[12px] text-[14px] leading-[20.3px] pl-6 "
+            className="rounded-[6px] outline-none border border-[#b3b3b3] md:w-[358px] text-[#b3b3b3] bg-[#000] flex pt-[10px] pb-[8px] px-[12px] items-center justify-center gap-[12px] text-[14px] leading-[20.3px] pl-10 "
           />
           <button
             type="submit"
             aria-label="Submit-search"
-            className="absolute left-2 top-[39%]"
+            className="absolute left-3 top-[29%]"
           >
             <svg
               fill="none"
               viewBox="0 0 13 14"
-              width="12"
+              width="20"
+              height={20}
               xmlns="http://www.w3.org/2000/svg"
               role="img"
               aria-label="Search-Icon"
@@ -39,7 +48,7 @@ const Header = () => {
             </svg>
           </button>
         </form>
-        <div className="text-[14px] font-[600] items-center justify-center text-[#b3b3b3] flex gap-6">
+        <div className="text-[14px] font-[600] hidden items-center justify-center text-[#b3b3b3] md:flex gap-6">
           <Link
             href="/"
             className="focus:text-[#ff5c00] active:scale-75 transform"
@@ -53,11 +62,135 @@ const Header = () => {
             Contact Us
           </Link>
         </div>
-        <div className="flex items-center justify-center gap-6">
+        <div className="hidden md:flex items-center justify-center gap-6">
           <Image src={User} alt="User icon" />
           <Image src={Cart} alt="Cart icon" />
         </div>
+        <div className="relative z-50 content md:hidden">
+          <motion.button
+            animate={mobileNav ? "open" : "closed"}
+            className="flex flex-col space-y-[5px] z-"
+            onClick={() => toggleMobileNav()}
+          >
+            <motion.span
+              variants={{
+                closed: { rotate: 0, y: 0 },
+                open: { rotate: 45, y: 7 },
+              }}
+              className="w-5 h-[2px] bg-[#b3b3b3] block rounded-full"
+            ></motion.span>
+            <motion.span
+              variants={{
+                closed: { opacity: 1 },
+                open: { opacity: 0 },
+              }}
+              className="w-5 h-[2px] bg-[#b3b3b3] block rounded-full"
+            ></motion.span>
+            <motion.span
+              variants={{
+                closed: { rotate: 0, y: 0 },
+                open: { rotate: -45, y: -7 },
+              }}
+              className="w-5 h-[2px] bg-[#b3b3b3] block rounded-full"
+            ></motion.span>
+          </motion.button>
+        </div>
       </div>
+      <AnimatePresence>
+        {mobileNav && (
+          <MotionConfig
+            transition={{
+              type: "spring",
+              bounce: 0,
+            }}
+          >
+            <motion.div
+              variants={{
+                open: {
+                  x: "70%",
+                  transition: {
+                    type: "spring",
+                    bounce: 0,
+                    when: "beforeChildren",
+                  },
+                },
+                closed: {
+                  x: "200%",
+                  transition: {
+                    type: "spring",
+                    bounce: 0.25,
+                    when: "afterChildren",
+                  },
+                },
+              }}
+              animate="open"
+              initial="closed"
+              exit="closed"
+              className="fixed inset-0 bg-white w-[80%] h-screen"
+            >
+              <button
+                onClick={() => toggleMobileNav()}
+                className="mt-8  ml-[130px] sm:ml-[230px]"
+              >
+                <X />
+              </button>
+              <motion.div className="px-8">
+                {" "}
+                <p
+                  className="cursor-pointer duration-700 text-black pt-5 pb-3"
+                  onClick={() => setIsDropDownVisible(!isDropDownVisible)}
+                >
+                  Features
+                  {isDropDownVisible ? (
+                    <ChevronUp className="inline mb-[1px]" />
+                  ) : (
+                    <ChevronDown className="inline mb-[1px]" />
+                  )}
+                  {isDropDownVisible && (
+                    <AnimatePresence>
+                      <motion.div
+                        variants={{
+                          open: {
+                            opacity: 1,
+                            transition: {
+                              duration: 0.2,
+                              ease: "easeInOut",
+                            },
+                          },
+                          closed: {
+                            opacity: 0,
+                            transition: {
+                              duration: 0.2,
+                              ease: "easeInOut",
+                            },
+                          },
+                        }}
+                        initial="closed"
+                        animate="open"
+                        exit="closed"
+                        className="flex flex-col bg-white text-black text-[14px] rounded-[8px]"
+                      >
+                        <Link
+                          className="rounded-t-[8px] pt-6 pb-3 hover:text-[#E0B0FF] duration-700"
+                          href="/"
+                        >
+                          Event Management
+                        </Link>
+                        <Link
+                          className="py-3 hover:text-[#E0B0FF] duration-700"
+                          href="/"
+                        >
+                          Guest Management
+                        </Link>
+                      </motion.div>
+                    </AnimatePresence>
+                  )}
+                </p>
+              </motion.div>
+            </motion.div>
+          </MotionConfig>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
--- src/app/components/herosection.tsx
+++ src/app/components/herosection.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import VectorLeft1 from "../../../public/vectorleft1.svg";
 import VectorLeft2 from "../../../public/vectorleft2.svg";
 import VectorRight1 from "../../../public/vectorright1.svg";
@@ -12,12 +14,12 @@ import Image from "next/image";
 
 const Herosection = () => {
   return (
-    <div className="mb-20 bg-[#fffafa] h-[800px] relative">
-      <div className="px-8 lg:px-10 pb-[150px] m-auto flex flex-col items-center justify-center text-center gap-6">
-        <p className="text-[40px] sm:text-[56px] w-full sm:w-[679px] gilroy font-[700] leading-[56px] text-[#060606] text-center ">
+    <div className="mb-20 bg-[#fffafa -10 h-[800px] reltive">
+      <div className="px-8 lg:px-10 pb-[150px] m-auto flex flex-col items-center justify-center text-center gap-6 pt-[100px] lg:pt-[130px]">
+        <p className="text-[40px] sm:text-[56px] w-full md:w-[679px] font-judson font-[700] leading-[56px] text-[#060606] text-center ">
           Shop quality and affordable fashion accessories{" "}
         </p>
-        <p className="text-[14px] w-full sm:w-[535px] font-[400] leading-[20.3px] text-[#060606] text-center ">
+        <p className="text-[14px] w-full sm:w-[535px] gilroy font-[400] leading-[20.3px] text-[#060606] text-center ">
           Get trendy shoes, bags, purses, and headties, and transform your
           everyday style into something extraordinary
         </p>
@@ -26,7 +28,7 @@ const Herosection = () => {
           style={{
             boxShadow: "0px 11px 22px 0px rgba(255, 92, 0, 0.25)",
           }}
-          className="text-[14px] text-[#fefefe] font-[600] z-50 active:scale-75 active:duration-100 transform flex py-[12px] px-[16px] items-center flex-col justify-center gap-[10px] rounded-[8px] bg-[#ff5c00] "
+          className="text-[14px] text-[#fefefe] font-[600] z-10 active:scale-75 active:duration-100 transform flex py-[12px] px-[16px] items-center flex-col justify-center gap-[10px] rounded-[8px] bg-[#ff5c00] "
         >
           Start Shopping
         </button>
@@ -56,19 +58,19 @@ const Herosection = () => {
             className="w-[160px] md:w-[100%]"
           />
         </div>
-        <div className="flex slides gap-4 flex-nowrap overflow-scroll float-right mt-[-40px] ">
-          <Image
-            src={Slide1}
-            alt="Vector"
-            className="h-[340px] rounded-lg z-50 rounded- border   "
-          />
+        <div className="flex slides gap-6 z-30 flex-nowrap overflow-scroll float-right mt-[-40px] ">
+          <Image src={Slide1} alt="Vector" className="h-[340px] rounded-lg  " />
           <Image src={Slide2} alt="Vector" className="h-[340px] rounded-lg " />
           <Image
             src={Slide3}
             alt="Vector"
             className="h-[255px] mt-[82px] rounded-lg "
           />
-          <Image src={Slide4} alt="Vector" className="h-[340px] rounded-lg " />
+          <Image
+            src={Slide4}
+            alt="Vector"
+            className="h-[320px] rounded-lg mt-[20px] "
+          />
           <Image
             src={Slide}
             alt="Vector"
