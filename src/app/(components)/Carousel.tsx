3555ffb50d2a1f88986282fb82e28956b236de50
theCephas <oshoiseoluwa@gmail.com>
2023-12-13 21:17:42 +0100

    Reusable Carousels

--- src/app/(components)/Carousel.tsx
+++ src/app/(components)/Carousel.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import React from "react";
-import Item1 from "../../../public/item1.svg";
-import Item2 from "../../../public/item2.svg";
-import Item3 from "../../../public/item3.svg";
-import Item4 from "../../../public/item4.svg";
-import Item5 from "../../../public/item5.svg";
-import Item6 from "../../../public/item6.svg";
-import Item7 from "../../../public/item7.svg";
-import ProductCard from "../(components)/ProductCard";
+import ProductCard from "./ProductCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const Handpicked: React.FC = (props) => {
+interface CarouselProps {
+  name: string;
+  slides: Array<{
+    id: number;
+    url: string;
+    name: string;
+    price: number;
+  }>;
+}
+
+const Carousel: React.FC<CarouselProps> = ({ name, slides }) => {
   function SampleNextArrow(props: {
     className: any;
     style: any;
@@ -67,10 +70,11 @@ const Handpicked: React.FC = (props) => {
     className: "center",
     infinite: true,
     slidesToShow: 6,
-    swipeToSlide: true,
-    afterChange: function (index: number) {
-      `Slider Changed to: ${index + 1}, background: #222; color: #bada55`;
-    },
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
     nextArrow: (
       <SampleNextArrow
         className={undefined}
@@ -90,25 +94,37 @@ const Handpicked: React.FC = (props) => {
         breakpoint: 1024,
         settings: {
           slidesToShow: 4,
-          slidesToScroll: 4,
+          slidesToScroll: 1,
           infinite: true,
           dots: true,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         },
       },
       {
         breakpoint: 800,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           initialSlide: 3,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         },
       },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         },
       },
       {
@@ -117,65 +133,25 @@ const Handpicked: React.FC = (props) => {
           slidesToShow: 1,
           slidesToScroll: 1,
           initialSlide: 1,
+          autoplay: true,
+          speed: 2000,
+          autoplaySpeed: 2000,
+          cssEase: "linear",
         },
       },
     ],
   };
 
-  const Slides = [
-    {
-      id: 1,
-      url: Item1,
-      name: "Nike Air Sneakers",
-      price: 17000,
-    },
-    {
-      id: 2,
-      url: Item2,
-      name: "Gucci Leather Bag",
-      price: 17000,
-    },
-    {
-      id: 3,
-      url: Item3,
-      name: 'Dior 6" heels',
-      price: 17000,
-    },
-    {
-      id: 4,
-      url: Item4,
-      name: "Ophidia GG Mediu...",
-      price: 17000,
-    },
-    {
-      id: 5,
-      url: Item6,
-      name: "Ego Heels",
-      price: 17000,
-    },
-    {
-      id: 6,
-      url: Item7,
-      name: "Prada spiky shoe",
-      price: 17000,
-    },
-    {
-      id: 7,
-      url: Item5,
-      name: "Nike Sneakers",
-      price: 17000,
-    },
-  ];
   return (
-    <div className="max-w-[1080px] mx-auto mb-[-80px] lg:mb-[-100px]">
+    <div className="max-w-[1080px] mx-auto pt-16">
       <div className="px-8 lg:px-0 ">
         <p className="gilroy text-[14px] text-[#040404] font-[600] leading-[20.3px] ">
-          Handpicked for you
+          {name}
         </p>
       </div>
       <div className="w-full m-auto ">
         <Slider {...settings} className="flex gap-20  ">
-          {Slides.map((item, index) => (
+          {slides.map((item, index) => (
             <div key={index} className="w-full m-auto py-10">
               <ProductCard
                 classes={"w-[160px] m-auto"}
@@ -191,4 +167,4 @@ const Handpicked: React.FC = (props) => {
   );
 };
 
-export default Handpicked;
+export default Carousel;
