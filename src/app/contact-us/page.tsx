6b524b42593866c2efeee0838c78b3cbc439c956
VictorIdowu <idsonvictor@gmail.com>
2023-12-15 11:13:06 +0100

    Start contact us page

---
+++ src/app/contact-us/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import PageWrapper from "../components/PageWrapper";
+
+const ContactUs = () => {
+  return (
+    <PageWrapper>
+      <div>Contact us</div>
+    </PageWrapper>
+  );
+};
+export default ContactUs;
