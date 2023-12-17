8f70f3ce83c9d61987f46853d18f239d9fc161b9
VictorIdowu <idsonvictor@gmail.com>
2023-12-17 18:27:02 +0100

    Added Animation

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { ReactNode } from "react";
 
 interface ModalProps {
@@ -11,15 +12,38 @@ const Modal = ({ isOpen, onClose, children }: ModalProps) => {
   if (!isOpen) return null;
 
   return (
-    <div className="fixed top-0 inset-0 z-50 flex items-center justify-center">
-      <div
-        onClick={onClose}
-        className="fixed inset-0 bg-black opacity-50"
-      ></div>
-      <div className="w-full h-full sm:w-[25.5rem] lg:w-[34rem] sm:ml-auto z-50 bg-white p-3 sm:p-6 lg:pr-20 overflow-scroll hide-scroll">
-        {children}
-      </div>
-    </div>
+    <AnimatePresence>
+      <motion.div
+        variants={{
+          open: {
+            opacity: 1,
+            transition: {
+              duration: 0.5,
+              ease: "easeInOut",
+            },
+          },
+          closed: {
+            opacity: 0,
+            transition: {
+              duration: 0.5,
+              ease: "easeInOut",
+            },
+          },
+        }}
+        initial="closed"
+        animate="open"
+        exit="closed"
+        className="fixed top-0 inset-0 z-50 flex items-center justify-center"
+      >
+        <div
+          onClick={onClose}
+          className="fixed inset-0 bg-black opacity-50"
+        ></div>
+        <div className="w-full h-full sm:w-[25.5rem] lg:w-[34rem] sm:ml-auto z-50 bg-white p-3 sm:p-6 lg:pr-20 overflow-scroll hide-scroll">
+          {children}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
