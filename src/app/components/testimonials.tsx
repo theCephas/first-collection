4eb77a20b3cb38e3a106a2be4a9da9b7cdf8a0f3
VictorIdowu <idsonvictor@gmail.com>
2023-12-25 13:50:16 +0100

    Prev button

--- src/app/components/testimonials.tsx
+++ src/app/components/testimonials.tsx
@@ -3,8 +3,7 @@
 import React, { useState } from "react";
 import Star from "../../../public/star.svg";
 import Image from "next/image";
-import { TestimonialDesign } from "./Icons";
-import { AnimatePresence, motion } from "framer-motion";
+import { BackIcon, TestimonialDesign } from "./Icons";
 
 type evidence = {
   id: number;
@@ -46,7 +45,6 @@ const Evidences: evidence[] = [
 ];
 
 const Testimonials = () => {
-  const [currentIndex, setCurrentIndex] = useState(0);
   const [evidences, setEvidences] = useState<evidence[]>(Evidences);
   const [transition, setTransition] = useState(false);
 
@@ -68,13 +66,26 @@ const Testimonials = () => {
     setEvidences(newEvidence);
   };
 
+  const handlePrev = async () => {
+    setTransition(true);
+    await wait(3);
+    let newEvidence: evidence[] = [];
+    evidences.forEach((_, index) => {
+      const newIndex =
+        index === 4 ? 0 : (index + 1 + evidences.length) % evidences.length;
+
+      newEvidence.push(evidences[newIndex]);
+    });
+
+    setTransition(false);
+
+    setEvidences(newEvidence);
+  };
+
   return (
     <div className="max-w-[1080px] mx-auto px-6 xl:px-0 py-[60px] flex flex-col items-center overflow-hidden">
       <div className="flex mb-16 w-full">
-        <p
-          onClick={handleNext}
-          className="font-judson w-[11.8rem] sm:w-[24rem] lg:w-[33rem] text-[18px] sm:text-[40px] font-[700] leading-[120%] text-[#060606] text-center m-auto pt-14 relative"
-        >
+        <p className="font-judson w-[11.8rem] sm:w-[24rem] lg:w-[33rem] text-[18px] sm:text-[40px] font-[700] leading-[120%] text-[#060606] text-center m-auto pt-14 relative">
           What our customers have said about our products
           <Image
             width={0}
@@ -88,26 +99,45 @@ const Testimonials = () => {
       <div className="flex ">
         {evidences.map((evidence, index) => (
           <div
-            key={index + 1}
-            className={`h-[10.6rem] min-w-[12rem] md:h-[12.6rem] ${
+            className={`flex gap-4 items-center relative ${
               index === 1 && "scale-75 sm:mr-10 blur-[1.5px]"
             } ${index === 3 && "scale-75 sm:ml-10 blur-[1.5px]"} ${
               index === 4 && "scale-50 blur-[2px]"
-            } ${
-              index === 0 && "scale-50 blur-[2px]"
-            } p-3 bg-black rounded-xl items-start gap-2.5 flex flex-col justify-between overflow-hidden relative ${
-              transition && "-translate-x-full"
-            }`}
+            } ${index === 0 && "scale-50 blur-[2px]"}`}
+            key={index + 1}
           >
-            <p className=" text-zinc-100 text-sm font-normal font-['Gilroy'] leading-tight">
-              {evidence.report}
-            </p>
-            <p className="text-neutral-200 text-sm font-semibold font-['Gilroy'] leading-tight">
-              {evidence.name}
-            </p>
-            <div className="absolute bottom-0 right-0">
-              <TestimonialDesign />
+            {index === 2 && (
+              <div
+                onClick={() => handlePrev()}
+                className="hidden w-6 h-6 p-1 bg-transparent rounded-3xl border border-neutral-100 justify-center items-center lg:flex z-50 cursor-pointer"
+              >
+                <BackIcon />
+              </div>
+            )}
+
+            <div
+              className={`h-[10.6rem] min-w-[12rem] md:h-[12.6rem]  p-3 bg-black rounded-xl items-start gap-2.5 flex flex-col justify-between overflow-hidden relative ${
+                transition && "-translate-x-full"
+              }`}
+            >
+              <p className=" text-zinc-100 text-sm font-normal font-['Gilroy'] leading-tight">
+                {evidence.report}
+              </p>
+              <p className="text-neutral-200 text-sm font-semibold font-['Gilroy'] leading-tight">
+                {evidence.name}
+              </p>
+              <div className="absolute bottom-0 right-0">
+                <TestimonialDesign />
+              </div>
             </div>
+            {index === 2 && (
+              <div
+                onClick={() => handleNext()}
+                className="hidden w-6 h-6 p-1 bg-transparent rounded-3xl border border-neutral-100 justify-center items-center lg:flex z-50 rotate-180 cursor-pointer"
+              >
+                <BackIcon />
+              </div>
+            )}
           </div>
         ))}
       </div>
